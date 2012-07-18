d25ff8bb8ea0ae49b0864f2739fd91af366a5f38
Giovanni Estrada Alemán <vagio12345@gmail.com>
2012-07-18 18:08:27 -0500

    Plantillas

---
+++ detalle-producto.php
@@ -0,0 +1,25 @@
+<?php	 
+	include('./core/util_helper.php');
+
+	//requiredincludes
+    require('./config/settings.php');
+			
+    //header (y/o menús)
+    $menues = TRUE;
+	
+	//incluir archivos js
+	$scripts = array();	
+	$scripts [] = TIENDA."js/login.js";
+	$scripts [] = TIENDA."js/registro.js";
+	
+	//header
+    require('./templates/header.php');
+    
+    //contenido
+    include('./components/detalle-producto.php');    
+    
+    //footer
+    require('./templates/footer.php');
+	
+?>
+
